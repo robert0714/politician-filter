4d4a6a4cfa2c25fbdbbbaaa0c200ba864447cd9a
robert.lee <robert0714@gmail.com>
2019-07-11 10:51:59 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# 政治人物 Filter
+
+The Chrome Extension to hide any post which is include "HanKuoYu" on Facebook.
+
+## Install By using Chrome Web Store
+
+
+## Install the Extension
+1. Clone the repo
+2. Access Chrome Extension Page
+3. Enable the "Developer mode" in the top-right corner
+3. Drag the Folder into Chrome
+
+## Notice
+If you found anything that can be improve, Please give me an issue, PR is better.
+
+## Standard
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
---
+++ script/watch.js
@@ -0,0 +1,35 @@
+// Author: Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
+// Github: https://github.com/maxchou415
+
+const blocklist = ['韓國瑜', '韓市長', '韓總', '國瑜', '韓流', '韓粉', '韓導', '郭台銘' ,'郭董','小英','蔡英文']
+const templateHtml = '<div><h1 style="padding: 30px; text-align: center;">草包已被隱藏！</h1></div>'
+
+const contentFromPosts = document.querySelector('#contentArea')
+const contentFromPages = document.querySelector('#pagelet_timeline_main_column')
+const content = contentFromPosts || contentFromPages
+
+function removeElems () {
+  const articles = content.querySelectorAll(`div[id][role="article"]`)
+
+  function hasSensitiveWordInBlocklist (article) {
+    return blocklist.some((sensitiveWord) => article.innerHTML.includes(sensitiveWord))
+  }
+
+  articles.forEach(function (article) {
+    if (hasSensitiveWordInBlocklist(article)) {
+      article.innerHTML = templateHtml
+    }
+  })
+}
+
+content.addEventListener('DOMContentLoaded', function (event) {
+  removeElems()
+})
+
+content.addEventListener('DOMNodeInserted', function (event) {
+  removeElems()
+})
+
+content.addEventListener('DOMSubtreeModified', function (event) {
+  removeElems()
+})
